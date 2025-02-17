6383832462bf3fd3aecdfe26334206dd56654448
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 19:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstChar(text) {
   // your code here
 	for(let i=0; i<text.length; i++){
-		if(text[i]==" " && text[i+1]){
+		if(text[i]==" " && text[i+1]==" "){
 			continue
 		}else if (text.length==0){
 			return "empty String"
-		}else if(text.length==1 && text[i]==" "){
+		}else if(text.length==1 && text[i]==' '){
 			return ''
 		}else{
 			return text[i]
