538cddf91992a0ab32c853d228f2baac40b0a0ff
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 19:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function firstChar(text) {
   // your code here
 	for(let i=0; i<text.length; i++){
-		if(text[i]==" " && text[i+1]==" "){
-			continue
-		}else if (text.length==0){
-			return "empty String"
-		}else if(text.length==1 && text[i]==' '){
-			return ''
-		}else{
+		if(text[i]!==" "){
 			return text[i]
 		}
 	}
+	return ""
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// //Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
